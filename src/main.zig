ebcfad99cfb4c4cb55ae1b63fd616347632f3a5f
Andrew Kelley <superjoe30@gmail.com>
2016-02-04 02:21:51 -0700

    drawing pieces

--- src/main.zig
+++ src/main.zig
@@ -8,13 +8,50 @@ import "libc.zig";
 import "all_shaders.zig";
 import "static_geometry.zig";
 import "debug_gl.zig";
+import "rand.zig";
+import "os.zig";
+import "pieces.zig";
 
 struct Tetris {
     window: &GLFWwindow,
     shaders: AllShaders,
     static_geometry: StaticGeometry,
     projection: Mat4x4,
+    rand: Rand,
+    piece_delay: f64,
+    delay_left: f64,
+    grid: [grid_height][grid_width]Cell,
+    piece_drop_count: i32,
+    cur_piece: &Piece,
+    cur_piece_x: i32,
+    cur_piece_y: i32,
+    cur_piece_rot: i32,
 }
+
+enum Cell {
+    Empty,
+    Color: Vec4,
+}
+
+const margin_size = 10;
+const grid_width = 10;
+const grid_height = 20;
+const cell_size = 32;
+const board_width = grid_width * cell_size;
+const board_height = grid_height * cell_size;
+const board_left = margin_size;
+const board_top = margin_size;
+const next_piece_width = margin_size + 4 * cell_size + margin_size;
+const next_piece_height = next_piece_width;
+const next_piece_left = board_left + board_width + margin_size;
+const next_piece_top = board_top + board_height - next_piece_height;
+const window_width = next_piece_left + next_piece_width + margin_size;
+const window_height = board_top + board_height + margin_size;
+
+const board_color = Vec4 { .data = []f32 {72.0/255.0, 72.0/255.0, 72.0/255.0, 1.0}};
+
+const init_piece_delay = 0.25;
+
 
 // TODO avoid having to make this function export
 export fn tetris_error_callback(err: c_int, description: ?&const u8) {
@@ -33,6 +70,7 @@ export fn main(argc: c_int, argv: &&u8) -> c_int {
     // TODO this crashes the compiler:
     // const gl_debug_on = if (@compile_var("is_release")) GL_FALSE else GL_TRUE;
 
+
     glfwSetErrorCallback(tetris_error_callback);
 
     if (glfwInit() == GL_FALSE) {
@@ -50,9 +88,9 @@ export fn main(argc: c_int, argv: &&u8) -> c_int {
     glfwWindowHint(GLFW_STENCIL_BITS, 8);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    var window = glfwCreateWindow(800, 450, c"Tetris", null, null) ?? {
-        glfwTerminate();
-        return -1;
+    var window = glfwCreateWindow(window_width, window_height, c"Tetris", null, null) ?? {
+        fprintf(stderr, c"unable to create window\n");
+        abort();
     };
 
     glfwSetKeyCallback(window, tetris_key_callback);
@@ -73,28 +111,54 @@ export fn main(argc: c_int, argv: &&u8) -> c_int {
     var width: c_int = undefined;
     var height: c_int = undefined;
     glfwGetFramebufferSize(window, &width, &height);
-    if (width < 800 || height < 450) unreachable{};
+    if (width < window_width || window_height < 450) unreachable{};
     const projection = mat4x4_ortho(0.0, f32(width), f32(height), 0.0);
     glViewport(0, 0, width, height);
+
+    const rand_seed = get_random_seed() %% {
+        fprintf(stderr, c"unable to get random seed\n");
+        abort();
+    };
 
     var t = Tetris {
         .window = window,
         .shaders = create_all_shaders(),
         .static_geometry = create_static_geometry(),
         .projection = projection,
+        .rand = rand_new(rand_seed),
+        .piece_delay = init_piece_delay,
+        .delay_left = init_piece_delay,
+        .piece_drop_count = 0,
+        .cur_piece = undefined,
+        .cur_piece_x = undefined,
+        .cur_piece_y = undefined,
+        .cur_piece_rot = undefined,
+        // TODO support the * operator for initilizing constant arrays
+        // then do: .grid =  ([1]Cell{Cell.Empty} * grid_width) * grid_height
+        .grid = undefined,
     };
+    init_empty_grid(&t);
+    drop_new_piece(&t);
+
     glfwSetWindowUserPointer(window, (&c_void)(&t));
 
     assert_no_gl_error();
 
+    const start_time = glfwGetTime();
+    var prev_time = start_time;
+
     while (glfwWindowShouldClose(window) == GL_FALSE) {
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
 
-        const blue = vec4(0.0, 0.0, 1.0, 1.0);
-        const scale = fabsf(sinf(f32(glfwGetTime())));
-        fill_rect(t, blue, 100.0, scale * 100.0, 50.0, 50.0);
+        const now_time = glfwGetTime();
+        const elapsed = now_time - prev_time;
+        prev_time = now_time;
 
+        next_frame(&t, elapsed);
+
+        draw(t);
         glfwSwapBuffers(window);
+
         glfwPollEvents();
     }
 
@@ -125,4 +189,62 @@ fn fill_rect(t: Tetris, color: Vec4, x: f32, y: f32, w: f32, h: f32) {
     const model = mat4x4_identity.translate(x, y, 0.0).scale(w, h, 0.0);
     const mvp = t.projection.mult(model);
     fill_rect_mvp(t, color, mvp);
+}
+
+fn get_random_seed() -> %u32 {
+    var seed : u32 = undefined;
+    const seed_bytes = (&u8)(&seed)[0...4];
+    %return os_get_random_bytes(seed_bytes);
+    return seed;
+}
+
+fn draw(t: Tetris) {
+    fill_rect(t, board_color, board_left, board_top, board_width, board_height);
+    fill_rect(t, board_color, next_piece_left, next_piece_top, next_piece_width, next_piece_height);
+
+    for (row, t.cur_piece.layout[t.cur_piece_rot], y) {
+        for (cell, row, x) {
+            const abs_x = t.cur_piece_x + x;
+            const abs_y = t.cur_piece_y + y;
+            if (abs_x >= 0 && abs_y >= 0 && abs_x < grid_width && abs_y < grid_height) {
+                const left = f32(board_left + cell_size * abs_x);
+                const top = f32(board_top + cell_size * abs_y);
+                if (cell) {
+                    fill_rect(t, t.cur_piece.color, left, top, cell_size, cell_size);
+                }
+            }
+        }
+    }
+}
+
+fn next_frame(t: &Tetris, elapsed: f64) {
+    t.delay_left -= elapsed;
+
+    if (t.delay_left <= 0) {
+        // fall
+        t.delay_left = t.piece_delay;
+    }
+
+}
+
+fn drop_new_piece(t: &Tetris) {
+    t.delay_left = t.piece_delay;
+    t.piece_drop_count += 1;
+
+    // TODO type generics so this doesn't have to be a u64
+    // TODO oops. super clumsy signedness casting here with rand_range and index operator
+    const index = t.rand.range_u64(0, u64(pieces.len));
+    t.cur_piece = &pieces[isize(index)];
+    t.cur_piece_x = 4;
+    t.cur_piece_y = 0;
+    t.cur_piece_rot = 0;
+}
+
+fn init_empty_grid(t: &Tetris) {
+    // TODO for loop over array with pointer
+    for (row, t.grid, y) {
+        for (cell, row, x) {
+            row[x] = Cell.Empty;
+        }
+    }
 }
---
+++ src/pieces.zig
@@ -0,0 +1,224 @@
+import "math3d.zig";
+
+pub struct Piece {
+    name: u8,
+    color: Vec4,
+    layout: [4][4][4]bool,
+}
+
+const _ = false;
+const O = true;
+
+pub const pieces = []Piece {
+    Piece {
+        .name = 'I',
+        .color = Vec4 { .data = []f32{ 0.0/255.0, 255.0/255.0, 255.0/255.0, 1.0 }, },
+        .layout = [][4][4]bool {
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ _, _, _, _ },
+                []bool{ O, O, O, O },
+                []bool{ _, _, _, _ },
+            },
+            [][4]bool {
+                []bool{ _, O, _, _ },
+                []bool{ _, O, _, _ },
+                []bool{ _, O, _, _ },
+                []bool{ _, O, _, _ },
+            },
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ _, _, _, _ },
+                []bool{ O, O, O, O },
+                []bool{ _, _, _, _ },
+            },
+            [][4]bool {
+                []bool{ _, O, _, _ },
+                []bool{ _, O, _, _ },
+                []bool{ _, O, _, _ },
+                []bool{ _, O, _, _ },
+            },
+        },
+    },
+    Piece {
+        .name = 'O',
+        .color = Vec4 { .data = []f32{ 255.0/255.0, 255.0/255.0, 0.0/255.0, 1.0 }, },
+        .layout = [][4][4]bool {
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ _, O, O, _ },
+                []bool{ _, O, O, _ },
+                []bool{ _, _, _, _ },
+            },
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ _, O, O, _ },
+                []bool{ _, O, O, _ },
+                []bool{ _, _, _, _ },
+            },
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ _, O, O, _ },
+                []bool{ _, O, O, _ },
+                []bool{ _, _, _, _ },
+            },
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ _, O, O, _ },
+                []bool{ _, O, O, _ },
+                []bool{ _, _, _, _ },
+            },
+        },
+    },
+    Piece {
+        .name = 'T',
+        .color = Vec4 { .data = []f32{ 255.0/255.0, 0.0/255.0, 255.0/255.0, 1.0 }, },
+        .layout = [][4][4]bool {
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ _, _, _, _ },
+                []bool{ O, O, O, _ },
+                []bool{ _, O, _, _ },
+            },
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ _, O, _, _ },
+                []bool{ O, O, _, _ },
+                []bool{ _, O, _, _ },
+            },
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ _, O, _, _ },
+                []bool{ O, O, O, _ },
+                []bool{ _, _, _, _ },
+            },
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ _, O, _, _ },
+                []bool{ _, O, O, _ },
+                []bool{ _, O, _, _ },
+            },
+        },
+    },
+    Piece {
+        .name = 'J',
+        .color = Vec4 { .data = []f32{ 0.0/255.0, 0.0/255.0, 255.0/255.0, 1.0 }, },
+        .layout = [][4][4]bool {
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ _, O, _, _ },
+                []bool{ _, O, _, _ },
+                []bool{ O, O, _, _ },
+            },
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ O, _, _, _ },
+                []bool{ O, O, O, _ },
+                []bool{ _, _, _, _ },
+            },
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ _, O, O, _ },
+                []bool{ _, O, _, _ },
+                []bool{ _, O, _, _ },
+            },
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ _, _, _, _ },
+                []bool{ O, O, O, _ },
+                []bool{ _, _, O, _ },
+            },
+        },
+    },
+    Piece {
+        .name = 'L',
+        .color = Vec4 { .data = []f32{ 255.0/255.0, 128.0/255.0, 0.0/255.0, 1.0 }, },
+        .layout = [][4][4]bool {
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ _, O, _, _ },
+                []bool{ _, O, _, _ },
+                []bool{ _, O, O, _ },
+            },
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ _, _, _, _ },
+                []bool{ O, O, O, _ },
+                []bool{ O, _, _, _ },
+            },
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ O, O, _, _ },
+                []bool{ _, O, _, _ },
+                []bool{ _, O, _, _ },
+            },
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ _, _, O, _ },
+                []bool{ O, O, O, _ },
+                []bool{ _, _, _, _ },
+            },
+        },
+    },
+    Piece {
+        .name = 'S',
+        .color = Vec4 { .data = []f32{ 0.0/255.0, 255.0/255.0, 0.0/255.0, 1.0 }, },
+        .layout = [][4][4]bool {
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ _, _, _, _ },
+                []bool{ _, O, O, _ },
+                []bool{ O, O, _, _ },
+            },
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ O, _, _, _ },
+                []bool{ O, O, _, _ },
+                []bool{ _, O, _, _ },
+            },
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ _, _, _, _ },
+                []bool{ _, O, O, _ },
+                []bool{ O, O, _, _ },
+            },
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ O, _, _, _ },
+                []bool{ O, O, _, _ },
+                []bool{ _, O, _, _ },
+            },
+        },
+    },
+    Piece {
+        .name = 'Z',
+        .color = Vec4 { .data = []f32{ 255.0/255.0, 0.0/255.0, 0.0/255.0, 1.0 }, },
+        .layout = [][4][4]bool {
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ _, _, _, _ },
+                []bool{ O, O, _, _ },
+                []bool{ _, O, O, _ },
+            },
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ _, _, O, _ },
+                []bool{ _, O, O, _ },
+                []bool{ _, O, _, _ },
+            },
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ _, _, _, _ },
+                []bool{ O, O, _, _ },
+                []bool{ _, O, O, _ },
+            },
+            [][4]bool {
+                []bool{ _, _, _, _ },
+                []bool{ _, _, O, _ },
+                []bool{ _, O, O, _ },
+                []bool{ _, O, _, _ },
+            },
+        },
+    },
+};
+
