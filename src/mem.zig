94cdcb21096b0cfc91efcd795012bbce128bc562
Andrew Kelley <superjoe30@gmail.com>
2017-09-05 18:51:31 -0400

    update to latest zig

--- src/mem.zig
+++ src/mem.zig
@@ -3,7 +3,7 @@ const c = @import("c.zig");
 error NoMem;
 
 pub fn alloc(comptime T: type, n: usize) -> %[]T {
-    const the_mem = c.malloc(c.size_t(n * @sizeOf(T))) ?? return error.NoMem;
+    const the_mem = c.malloc(n * @sizeOf(T)) ?? return error.NoMem;
     const aligned = @alignCast(8, the_mem);
     return @ptrCast(&T, aligned)[0..n];
 }
