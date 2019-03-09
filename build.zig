1fd392b5b70f4c471826222204c5dbb48ef535e8
Andrew Kelley <andrew@ziglang.org>
2019-03-09 01:20:13 -0500

    fix build script
    closes #3

--- build.zig
+++ build.zig
@@ -9,7 +9,7 @@ pub fn build(b: *Builder) void {
     exe.setBuildMode(mode);
 
     if (windows) {
-        exe.setTarget(builtin.Arch.x86_64, builtin.Os.windows, builtin.Environ.gnu);
+        exe.setTarget(builtin.Arch.x86_64, builtin.Os.windows, builtin.Abi.gnu);
     }
 
     exe.linkSystemLibrary("c");
@@ -24,7 +24,6 @@ pub fn build(b: *Builder) void {
     b.installArtifact(exe);
 
     const play = b.step("play", "Play the game");
-    const run = b.addCommand(".", b.env_map, [][]const u8{exe.getOutputPath()});
+    const run = exe.run();
     play.dependOn(&run.step);
-    run.step.dependOn(&exe.step);
 }
