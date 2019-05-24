f62ecc0825ae2713bdf33681d0fa10c0f43b6401
Merlyn Morgan-Graham <kavika@gmail.com>
2019-05-23 20:33:47 -0700

    Convert to use stb_image, remove dependency on libpng and zlib

--- src/c.zig
+++ src/c.zig
@@ -1,7 +1,9 @@
 pub use @cImport({
     @cInclude("epoxy/gl.h");
     @cInclude("GLFW/glfw3.h");
-    @cInclude("png.h");
+    @cDefine("STBI_ONLY_PNG", "");
+    @cDefine("STBI_NO_STDIO", "");
+    @cInclude("stb_image.h");
     @cInclude("math.h");
     @cInclude("stdlib.h");
     @cInclude("stdio.h");
--- src/png.zig
+++ src/png.zig
@@ -1,5 +1,4 @@
 const c = @import("c.zig");
-const c_allocator = @import("std").heap.c_allocator;
 
 pub const PngImage = struct {
     width: u32,
@@ -8,90 +7,39 @@ pub const PngImage = struct {
     raw: []u8,
 
     pub fn destroy(pi: *PngImage) void {
-        c_allocator.free(pi.raw);
+        c.stbi_image_free(pi.raw.ptr);
     }
 
     pub fn create(compressed_bytes: []const u8) !PngImage {
         var pi: PngImage = undefined;
 
-        if (c.png_sig_cmp(compressed_bytes.ptr, 0, 8) != 0) {
+        var width: c_int = undefined;
+        var height: c_int = undefined;
+
+        if (c.stbi_info_from_memory(compressed_bytes.ptr, @intCast(c_int, compressed_bytes.len), &width, &height, null) == 0) {
             return error.NotPngFile;
         }
 
-        var png_ptr = c.png_create_read_struct(c.PNG_LIBPNG_VER_STRING, null, null, null);
-        if (png_ptr == null) return error.NoMem;
+        if (width <= 0 or height <= 0) return error.NoPixels;
+        pi.width = @intCast(u32, width);
+        pi.height = @intCast(u32, height);
 
-        var info_ptr = c.png_create_info_struct(png_ptr);
-        if (info_ptr == null) {
-            c.png_destroy_read_struct(&png_ptr, 0, 0);
-            return error.NoMem;
+        // Not validating channel_count because it gets auto-converted to 4
+
+        if (c.stbi_is_16_bit_from_memory(compressed_bytes.ptr, @intCast(c_int, compressed_bytes.len)) != 0) {
+            return error.InvalidFormat;
         }
-        defer c.png_destroy_read_struct(&png_ptr, &info_ptr, 0);
+        const bits_per_channel = 8;
+        const channel_count = 4;
 
-        //// don't call any png_* functions outside of this function.
-        //// cursed is he who thought setjmp and longjmp was in any way acceptable to
-        //// bake into a library API.
-        //if (setjmp((?&struct___jmp_buf_tag)(png_set_longjmp_fn(png_ptr, longjmp, @sizeOf(jmp_buf)))) != 0) {
-        //    return error.InvalidFormat;
-        //}
+        c.stbi_set_flip_vertically_on_load(1);
+        const image_data = c.stbi_load_from_memory(compressed_bytes.ptr, @intCast(c_int, compressed_bytes.len), &width, &height, null, channel_count);
 
-        c.png_set_sig_bytes(png_ptr, 8);
-
-        var png_io = PngIo{
-            .index = 8,
-            .buffer = compressed_bytes,
-        };
-        c.png_set_read_fn(png_ptr, @ptrCast(*c_void, &png_io), readPngData);
-
-        c.png_read_info(png_ptr, info_ptr);
-
-        pi.width = c.png_get_image_width(png_ptr, info_ptr);
-        pi.height = c.png_get_image_height(png_ptr, info_ptr);
-
-        if (pi.width <= 0 or pi.height <= 0) return error.NoPixels;
-
-        // bits per channel (not per pixel)
-        const bits_per_channel = c.png_get_bit_depth(png_ptr, info_ptr);
-        if (bits_per_channel != 8) return error.InvalidFormat;
-
-        const channel_count = c.png_get_channels(png_ptr, info_ptr);
-        if (channel_count != 4) return error.InvalidFormat;
-
-        const color_type = c.png_get_color_type(png_ptr, info_ptr);
-        if (color_type != PNG_COLOR_TYPE_RGBA) return error.InvalidFormat;
+        if (image_data == null) return error.NoMem;
 
         pi.pitch = pi.width * bits_per_channel * channel_count / 8;
-        pi.raw = try c_allocator.alloc(u8, pi.height * pi.pitch);
-        errdefer c_allocator.free(pi.raw);
-
-        const row_ptrs = try c_allocator.alloc(c.png_bytep, pi.height);
-        defer c_allocator.free(row_ptrs);
-
-        {
-            var i: usize = 0;
-            while (i < pi.height) : (i += 1) {
-                const q = (pi.height - i - 1) * pi.pitch;
-                row_ptrs[i] = pi.raw.ptr + q;
-            }
-        }
-
-        c.png_read_image(png_ptr, row_ptrs.ptr);
+        pi.raw = image_data[0..pi.height * pi.pitch];
 
         return pi;
     }
 };
-
-const PngIo = struct {
-    index: usize,
-    buffer: []const u8,
-};
-
-extern fn readPngData(png_ptr: c.png_structp, data: c.png_bytep, length: c.png_size_t) void {
-    const png_io = @ptrCast(*PngIo, @alignCast(@alignOf(PngIo), c.png_get_io_ptr(png_ptr).?));
-    const new_index = png_io.index + length;
-    if (new_index > png_io.buffer.len) unreachable;
-    @memcpy(@ptrCast([*]u8, data), png_io.buffer.ptr + png_io.index, length);
-    png_io.index = new_index;
-}
-
-const PNG_COLOR_TYPE_RGBA = c.PNG_COLOR_MASK_COLOR | c.PNG_COLOR_MASK_ALPHA;
