92c2c274c8dd790e1452d937ca3fab203308f49a
markfirmware <markfirmware@users.noreply.github.com>
2019-06-22 11:09:48 -0400

    update array init syntax with [_]

--- src/pieces.zig
+++ src/pieces.zig
@@ -9,228 +9,228 @@ pub const Piece = struct {
 const F = false;
 const T = true;
 
-pub const pieces = []Piece{
+pub const pieces = [_]Piece{
     Piece{
         .name = 'I',
         .color = Vec4{
-            .data = []f32{ 0.0 / 255.0, 255.0 / 255.0, 255.0 / 255.0, 1.0 },
+            .data = [_]f32{ 0.0 / 255.0, 255.0 / 255.0, 255.0 / 255.0, 1.0 },
         },
-        .layout = [][4][4]bool{
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ F, F, F, F },
-                []bool{ T, T, T, T },
-                []bool{ F, F, F, F },
+        .layout = [_][4][4]bool{
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, F, F, F },
+                [_]bool{ T, T, T, T },
+                [_]bool{ F, F, F, F },
             },
-            [][4]bool{
-                []bool{ F, T, F, F },
-                []bool{ F, T, F, F },
-                []bool{ F, T, F, F },
-                []bool{ F, T, F, F },
+            [_][4]bool{
+                [_]bool{ F, T, F, F },
+                [_]bool{ F, T, F, F },
+                [_]bool{ F, T, F, F },
+                [_]bool{ F, T, F, F },
             },
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ F, F, F, F },
-                []bool{ T, T, T, T },
-                []bool{ F, F, F, F },
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, F, F, F },
+                [_]bool{ T, T, T, T },
+                [_]bool{ F, F, F, F },
             },
-            [][4]bool{
-                []bool{ F, T, F, F },
-                []bool{ F, T, F, F },
-                []bool{ F, T, F, F },
-                []bool{ F, T, F, F },
-            },
-        },
-    },
-    Piece{
-        .name = 'T',
-        .color = Vec4{
-            .data = []f32{ 255.0 / 255.0, 255.0 / 255.0, 0.0 / 255.0, 1.0 },
-        },
-        .layout = [][4][4]bool{
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ F, F, F, F },
-                []bool{ T, T, F, F },
-                []bool{ T, T, F, F },
-            },
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ F, F, F, F },
-                []bool{ T, T, F, F },
-                []bool{ T, T, F, F },
-            },
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ F, F, F, F },
-                []bool{ T, T, F, F },
-                []bool{ T, T, F, F },
-            },
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ F, F, F, F },
-                []bool{ T, T, F, F },
-                []bool{ T, T, F, F },
+            [_][4]bool{
+                [_]bool{ F, T, F, F },
+                [_]bool{ F, T, F, F },
+                [_]bool{ F, T, F, F },
+                [_]bool{ F, T, F, F },
             },
         },
     },
     Piece{
         .name = 'T',
         .color = Vec4{
-            .data = []f32{ 255.0 / 255.0, 0.0 / 255.0, 255.0 / 255.0, 1.0 },
+            .data = [_]f32{ 255.0 / 255.0, 255.0 / 255.0, 0.0 / 255.0, 1.0 },
         },
-        .layout = [][4][4]bool{
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ F, F, F, F },
-                []bool{ T, T, T, F },
-                []bool{ F, T, F, F },
+        .layout = [_][4][4]bool{
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, F, F, F },
+                [_]bool{ T, T, F, F },
+                [_]bool{ T, T, F, F },
             },
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ F, T, F, F },
-                []bool{ T, T, F, F },
-                []bool{ F, T, F, F },
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, F, F, F },
+                [_]bool{ T, T, F, F },
+                [_]bool{ T, T, F, F },
             },
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ F, T, F, F },
-                []bool{ T, T, T, F },
-                []bool{ F, F, F, F },
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, F, F, F },
+                [_]bool{ T, T, F, F },
+                [_]bool{ T, T, F, F },
             },
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ F, T, F, F },
-                []bool{ F, T, T, F },
-                []bool{ F, T, F, F },
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, F, F, F },
+                [_]bool{ T, T, F, F },
+                [_]bool{ T, T, F, F },
+            },
+        },
+    },
+    Piece{
+        .name = 'T',
+        .color = Vec4{
+            .data = [_]f32{ 255.0 / 255.0, 0.0 / 255.0, 255.0 / 255.0, 1.0 },
+        },
+        .layout = [_][4][4]bool{
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, F, F, F },
+                [_]bool{ T, T, T, F },
+                [_]bool{ F, T, F, F },
+            },
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, T, F, F },
+                [_]bool{ T, T, F, F },
+                [_]bool{ F, T, F, F },
+            },
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, T, F, F },
+                [_]bool{ T, T, T, F },
+                [_]bool{ F, F, F, F },
+            },
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, T, F, F },
+                [_]bool{ F, T, T, F },
+                [_]bool{ F, T, F, F },
             },
         },
     },
     Piece{
         .name = 'J',
         .color = Vec4{
-            .data = []f32{ 0.0 / 255.0, 0.0 / 255.0, 255.0 / 255.0, 1.0 },
+            .data = [_]f32{ 0.0 / 255.0, 0.0 / 255.0, 255.0 / 255.0, 1.0 },
         },
-        .layout = [][4][4]bool{
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ F, T, F, F },
-                []bool{ F, T, F, F },
-                []bool{ T, T, F, F },
+        .layout = [_][4][4]bool{
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, T, F, F },
+                [_]bool{ F, T, F, F },
+                [_]bool{ T, T, F, F },
             },
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ T, F, F, F },
-                []bool{ T, T, T, F },
-                []bool{ F, F, F, F },
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ T, F, F, F },
+                [_]bool{ T, T, T, F },
+                [_]bool{ F, F, F, F },
             },
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ F, T, T, F },
-                []bool{ F, T, F, F },
-                []bool{ F, T, F, F },
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, T, T, F },
+                [_]bool{ F, T, F, F },
+                [_]bool{ F, T, F, F },
             },
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ F, F, F, F },
-                []bool{ T, T, T, F },
-                []bool{ F, F, T, F },
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, F, F, F },
+                [_]bool{ T, T, T, F },
+                [_]bool{ F, F, T, F },
             },
         },
     },
     Piece{
         .name = 'L',
         .color = Vec4{
-            .data = []f32{ 255.0 / 255.0, 128.0 / 255.0, 0.0 / 255.0, 1.0 },
+            .data = [_]f32{ 255.0 / 255.0, 128.0 / 255.0, 0.0 / 255.0, 1.0 },
         },
-        .layout = [][4][4]bool{
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ F, T, F, F },
-                []bool{ F, T, F, F },
-                []bool{ F, T, T, F },
+        .layout = [_][4][4]bool{
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, T, F, F },
+                [_]bool{ F, T, F, F },
+                [_]bool{ F, T, T, F },
             },
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ F, F, F, F },
-                []bool{ T, T, T, F },
-                []bool{ T, F, F, F },
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, F, F, F },
+                [_]bool{ T, T, T, F },
+                [_]bool{ T, F, F, F },
             },
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ T, T, F, F },
-                []bool{ F, T, F, F },
-                []bool{ F, T, F, F },
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ T, T, F, F },
+                [_]bool{ F, T, F, F },
+                [_]bool{ F, T, F, F },
             },
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ F, F, T, F },
-                []bool{ T, T, T, F },
-                []bool{ F, F, F, F },
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, F, T, F },
+                [_]bool{ T, T, T, F },
+                [_]bool{ F, F, F, F },
             },
         },
     },
     Piece{
         .name = 'S',
         .color = Vec4{
-            .data = []f32{ 0.0 / 255.0, 255.0 / 255.0, 0.0 / 255.0, 1.0 },
+            .data = [_]f32{ 0.0 / 255.0, 255.0 / 255.0, 0.0 / 255.0, 1.0 },
         },
-        .layout = [][4][4]bool{
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ F, F, F, F },
-                []bool{ F, T, T, F },
-                []bool{ T, T, F, F },
+        .layout = [_][4][4]bool{
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, T, T, F },
+                [_]bool{ T, T, F, F },
             },
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ T, F, F, F },
-                []bool{ T, T, F, F },
-                []bool{ F, T, F, F },
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ T, F, F, F },
+                [_]bool{ T, T, F, F },
+                [_]bool{ F, T, F, F },
             },
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ F, F, F, F },
-                []bool{ F, T, T, F },
-                []bool{ T, T, F, F },
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, T, T, F },
+                [_]bool{ T, T, F, F },
             },
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ T, F, F, F },
-                []bool{ T, T, F, F },
-                []bool{ F, T, F, F },
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ T, F, F, F },
+                [_]bool{ T, T, F, F },
+                [_]bool{ F, T, F, F },
             },
         },
     },
     Piece{
         .name = 'Z',
         .color = Vec4{
-            .data = []f32{ 255.0 / 255.0, 0.0 / 255.0, 0.0 / 255.0, 1.0 },
+            .data = [_]f32{ 255.0 / 255.0, 0.0 / 255.0, 0.0 / 255.0, 1.0 },
         },
-        .layout = [][4][4]bool{
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ F, F, F, F },
-                []bool{ T, T, F, F },
-                []bool{ F, T, T, F },
+        .layout = [_][4][4]bool{
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, F, F, F },
+                [_]bool{ T, T, F, F },
+                [_]bool{ F, T, T, F },
             },
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ F, F, T, F },
-                []bool{ F, T, T, F },
-                []bool{ F, T, F, F },
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, F, T, F },
+                [_]bool{ F, T, T, F },
+                [_]bool{ F, T, F, F },
             },
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ F, F, F, F },
-                []bool{ T, T, F, F },
-                []bool{ F, T, T, F },
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, F, F, F },
+                [_]bool{ T, T, F, F },
+                [_]bool{ F, T, T, F },
             },
-            [][4]bool{
-                []bool{ F, F, F, F },
-                []bool{ F, F, T, F },
-                []bool{ F, T, T, F },
-                []bool{ F, T, F, F },
+            [_][4]bool{
+                [_]bool{ F, F, F, F },
+                [_]bool{ F, F, T, F },
+                [_]bool{ F, T, T, F },
+                [_]bool{ F, T, F, F },
             },
         },
     },
