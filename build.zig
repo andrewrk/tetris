6b74c8fe7bd6fcb68b3fab197e43736872f514c2
Daniel Salvadori <danaugrs@gmail.com>
2023-04-08 12:23:40 -0300

    Use latest Zig

--- build.zig
+++ build.zig
@@ -1,28 +1,26 @@
 const std = @import("std");
 const Builder = std.build.Builder;
+const Build = std.build;
 
 pub fn build(b: *Builder) void {
-    const mode = b.standardReleaseOptions();
-    const windows = b.option(bool, "windows", "create windows build") orelse false;
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     const vcpkg = b.option(bool, "vcpkg", "Add vcpkg paths to the build") orelse false;
 
-    var exe = b.addExecutable("tetris", "src/main.zig");
+    var exe = b.addExecutable(.{
+        .name = "tetris",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .optimize = optimize,
+        .target = target,
+    });
     exe.addCSourceFile("stb_image-2.22/stb_image_impl.c", &[_][]const u8{"-std=c99"});
-    exe.setBuildMode(mode);
-
-    if (windows) {
-        exe.setTarget(.{
-            .cpu_arch = .x86_64,
-            .os_tag = .windows,
-            .abi = .gnu,
-        });
-    }
 
     if (vcpkg) {
         exe.addVcpkgPaths(.static) catch @panic("Cannot add vcpkg paths.");
     }
 
-    exe.addIncludeDir("stb_image-2.22");
+    exe.addIncludePath("stb_image-2.22");
 
     exe.linkSystemLibrary("c");
     exe.linkSystemLibrary("glfw");
