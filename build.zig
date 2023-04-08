70b9746dd18808a4baedd4a33ef5e692c6c10019
Andrew Kelley <andrew@ziglang.org>
2023-04-08 08:46:59 -0700

    clean up build.zig

--- build.zig
+++ build.zig
@@ -1,24 +1,17 @@
 const std = @import("std");
-const Builder = std.build.Builder;
 const Build = std.build;
 
-pub fn build(b: *Builder) void {
+pub fn build(b: *Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const vcpkg = b.option(bool, "vcpkg", "Add vcpkg paths to the build") orelse false;
-
-    var exe = b.addExecutable(.{
+    const exe = b.addExecutable(.{
         .name = "tetris",
         .root_source_file = .{ .path = "src/main.zig" },
         .optimize = optimize,
         .target = target,
     });
     exe.addCSourceFile("stb_image-2.22/stb_image_impl.c", &[_][]const u8{"-std=c99"});
-
-    if (vcpkg) {
-        exe.addVcpkgPaths(.static) catch @panic("Cannot add vcpkg paths.");
-    }
 
     exe.addIncludePath("stb_image-2.22");
 
