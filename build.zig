e6aa7c551204297df744e53cef3e7c1d15190562
Jakub Konka <kubkon@jakubkonka.com>
2023-10-20 20:38:18 +0200

    remove the last remaining workaround for -Duse-llvm=false
    Fixed in Zig in https://github.com/ziglang/zig/pull/17621

--- build.zig
+++ build.zig
@@ -15,9 +15,6 @@ pub fn build(b: *Build) void {
     });
     exe.use_llvm = use_llvm;
     exe.use_lld = use_llvm;
-    if (use_llvm == false) {
-        exe.pie = true;
-    }
 
     exe.linkLibC();
     exe.linkSystemLibrary("glfw");
