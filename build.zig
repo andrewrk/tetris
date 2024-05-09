689ae93b4531365b8646d7db53cb0c924c44014a
Andrew Kelley <andrew@ziglang.org>
2024-05-09 14:49:10 -0700

    build.zig: use the options for setting the use_llvm flags

--- build.zig
+++ build.zig
@@ -3,7 +3,6 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-
     const use_llvm = b.option(bool, "use-llvm", "use the LLVM backend");
 
     const exe = b.addExecutable(.{
@@ -11,9 +10,9 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/main.zig"),
         .optimize = optimize,
         .target = target,
+        .use_llvm = use_llvm,
+        .use_lld = use_llvm,
     });
-    exe.use_llvm = use_llvm;
-    exe.use_lld = use_llvm;
 
     exe.linkLibC();
     exe.linkSystemLibrary("glfw");
